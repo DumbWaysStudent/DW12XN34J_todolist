016fceef5f6995b0c2c0a247b87c0a8bf28b9753
sandyadzim <sandy.adzim@gmail.com>
2019-10-07 14:53:00 +0700

    2.add_todo

--- App.js
+++ App.js
@@ -1,34 +1,73 @@
 import React, { Component } from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
-import { Container } from 'native-base';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Button } from 'react-native'
 
-class ListDo extends Component {
+class Add extends Component {
 
   constructor(props) {
     super(props)
 
+    //list data
     this.state = {
-      data: [
+      datas: [
         'work',
         'swim',
         'study',
         'sleep',
         'run',
-      ]
+      ],
+      data: '',
     }
+  }
+
+  //inputan data
+  onHandleAdd = (text) => {
+    this.setState({data: text})
+  }
+
+  //masukkan inputan data ke list
+  onHandleBtn = () =>{
+    if(this.state.data !== ''){
+      let tambah = this.state.datas.concat(this.state.data)
+      this.setState({datas: tambah})
+      this.setState({data: ''})
+    }else{
+      alert('Field Tidak boleh kosong!')
+    }
+
   }
 
 
   render() {
 
     return (
-      <Container>
-        <View>
 
-          {this.state.data.map((item) => <Text style={styles.textList}>{item}</Text>)}
+      <View>
+        
+        <View style={styles.direc}>
+              
+          <TextInput style={styles.inputTxt}
+            placeholder='New todo'
+            type='text'
+            onChangeText={(text) => this.onHandleAdd(text)} value={this.state.data}
+          />
 
+          <TouchableOpacity style={styles.btn}>
+              <Text
+              style={styles.txtB}
+              onPress={this.onHandleBtn}
+              >Add</Text>
+          </TouchableOpacity>
+
+                 
         </View>
-      </Container>
+
+            {this.state.datas.map((item) => 
+            <Text style={styles.textList}>{item}</Text>)
+            }
+
+      </View>
+          
+      
     )
   }
 
@@ -38,7 +77,35 @@ const styles = StyleSheet.create({
   textList: {
     fontSize: 28,
     borderBottomWidth: 2,
-  }
+  },
+  inputTxt:{
+    width:'80%',
+    borderColor:'red',
+    height:50,
+    borderWidth:2,
+    fontSize:18,
+
+  },
+  btn:{
+    borderRadius:2,
+    backgroundColor:'blue',
+    width:70,
+    height:50,
+  },
+  txtB:{
+    textAlign:'center',
+    justifyContent:'center',
+    fontWeight:'bold',
+    fontSize:24,
+    paddingTop:7,
+    },
+  direc:{
+    flexDirection:'row',
+    justifyContent:'center',
+    paddingBottom:50,
+    alignItems:'center',
+    
+  },
 })
 
-export default ListDo;
+export default Add;
