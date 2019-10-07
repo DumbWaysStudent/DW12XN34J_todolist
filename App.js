272e676167d92e93e6f1bc50471b009982d7bc2a
sandyadzim <sandy.adzim@gmail.com>
2019-10-07 19:50:42 +0700

    remove import yg tidak perlu

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { Container } from 'native-base';
 
 class ListDo extends Component {
